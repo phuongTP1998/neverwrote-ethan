bb9d498882388896a7ee507137fc4c42799788ae
Ethan <popping1011@gmail.com>
2019-10-12 12:34:58 +1100

    first commit, api notebooks changed

--- api/src/controllers/notebooks.js
+++ api/src/controllers/notebooks.js
@@ -4,12 +4,28 @@ const models = require('../models');
 
 const router = express.Router();
 
+function notebookFilter(obj){
+  return _.pick(obj, ["title"])
+}
+
 // Index
 router.get('/', (req, res) => {
   models.Notebook.findAll({ order: [['createdAt', 'DESC']] })
     .then(notebooks => res.json(notebooks))
     .catch(err => res.status(500).json({ error: err.message }));
 });
+
+router.get('/:notebookId/notes', (req, res) => {
+  models.Note.findAll({ where : { notebookId: req.params.notebookId }})
+    .then(note => res.json(note))
+    .catch(err => res.status(500).json({error: err.message}));
+})
+
+router.post('/', (req, res) => {
+  models.Notebook.create(notebookFilter(req.body))
+    .then(notebook => res.json(notebook))
+    .catch(err => res.status(422).json({ error: err.message }))
+})
 
 /* *** TODO: Fill in the API endpoints for notebooks *** */
 
--- api/src/controllers/notes.js
+++ api/src/controllers/notes.js
@@ -5,5 +5,10 @@ const models = require('../models');
 const router = express.Router();
 
 /* *** TODO: Fill in the API endpoints for notes *** */
+router.get("/", (req, res) => {
+  models.Note.findAll({ order: [["createdAt", "DESC"]] })
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
 
 module.exports = router;
